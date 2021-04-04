c49f86aa79c66cd81f4f77c88ca6f9df291d35bd
octex <56984557+octex@users.noreply.github.com>
2021-04-04 16:42:35 -0300

    CRUD controles completo

--- app.py
+++ app.py
@@ -9,25 +9,31 @@ from constants import HTTPMethods
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
+
 @app.route('/', methods=[HTTPMethods.GET])
 def home():
     return {}
 
-@app.route('/controles/', methods=[HTTPMethods.GET, HTTPMethods.POST])
+
+@app.route('/controles/', methods=[HTTPMethods.GET, HTTPMethods.POST, HTTPMethods.PUT])
 def controles():
     return controles_manager(request)
+
 
 @app.route('/insumos/')
 def insumos():
     return {}
 
+
 @app.route('/analisis/')
 def analisis():
     return {}
 
+
 @app.route('/turnos/')
 def turnos():
     return {}
+
 
 @app.route('/informes/')
 def informes():
