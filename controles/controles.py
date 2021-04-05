b1202e519e8b2d1de5877193d6b28336e1bc7f09
octex <56984557+octex@users.noreply.github.com>
2021-04-05 19:42:51 -0300

    Query params funcando!

--- controles/controles.py
+++ controles/controles.py
@@ -47,19 +47,27 @@ def fecha_parser(fecha):
     return new_date
 
 
+def query_params_parser(query_params, possible_params):
+    new_query_params = {}
+    for param in query_params:
+        if param in possible_params:
+            new_query_params[param] = query_params[param]
+    return new_query_params
+
+
 def get_controles(request):
     query_params = request.args
+    possible_params = ["valor", "unidades", "fecha", "comida"]
+    actual_params = query_params_parser(query_params=query_params, possible_params=possible_params)
     session = Session()
-    if len(query_params) == 0:
+    if len(query_params) == 0 or len(actual_params) == 0:
         controles = session.query(Control).all()
         controles = controles_parser(controles)
     else:
-        tmp_q_p = {
-            "fecha": "",
-            "comida": "",
-            "valor": "",
-            "unidades": ""
-        }
+        controles = session.query(Control).filter_by(**actual_params).all()
+        controles = controles_parser(controles)
+        # if len(actual_params) == len(possible_params):
+            # controles = session.query(Control).filter_by().all()
     session.close()
     return jsonify({"Controles": controles, "Registros": len(controles)})
 
