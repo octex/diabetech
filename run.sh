686c07401587fca3fb75b35d1e734a40f391a399
octex <okilauta@hotmail.com>
2022-08-26 22:20:41 -0300

    Fixed run script file

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 clear
 cd /home/pi/Desktop/projects/diabetech/
 export FLASK_APP=app
-export FLASK_ENV=development
+echo '________  .__      ___.           __                .__'
+echo '\______ \ |__|____ \_ |__   _____/  |_  ____   ____ |  |__  '
+echo ' |    |  \|  \__  \ | __ \_/ __ \   __\/ __ \_/ ___\|  |  \ '
+echo ' |    `   \  |/ __ \| \_\ \  ___/|  | \  ___/\  \___|   Y  \'
+echo '/_______  /__(____  /___  /\___  >__|  \___  >\___  >___|  /'
+echo '        \/        \/    \/     \/          \/     \/     \/ '
 flask run --host=0.0.0.0
